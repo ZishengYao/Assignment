8ad61a3b617e6a0efa3f7091ee4ca9896490d54d
ZishengYao <65621968+ZishengYao@users.noreply.github.com>
2022-04-14 18:25:55 -0400

    Create rewards.js

---
+++ rewards.js
@@ -0,0 +1,43 @@
+const record = {
+    Janurary:{
+        1:123,
+        2:215,
+        3:313,
+        4:51,
+        6:72,
+        7:93,
+        8:121,
+        9:620,
+        10:1210
+    },
+    Feburary:{
+        1:312,
+        2:55,
+        3:132,
+        4:921,
+        6:321,
+        7:344,
+        8:112,
+        9:322,
+        10:555
+    }
+}
+const calculateRewards=(record)=>{
+    let ans = {}
+    Object.keys(record).map((month)=>{
+        let monlyRewards=0
+        const monthlyData= record[month]
+        Object.keys(monthlyData).map((ele)=>{
+            const purchase= monthlyData[ele]
+            if(purchase>50&&purchase<=100){
+                monlyRewards+= purchase-50
+            }else if(purchase>100){
+                monlyRewards+= 50 + 2*(purchase-100)
+            }
+        })
+        ans[month]=monlyRewards
+    })
+    return ans
+}
+
+console.log(calculateRewards(record))
